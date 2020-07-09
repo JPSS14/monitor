bc318c8c4ef7e4f2d901ea36437f8bf1284d1d18
JPSS14 <jp_scarabelli@hotmail.com>
2020-07-08 23:57:08 -0300

    monitor UPDATE 2: Added theme style

--- Monitor/index.php
+++ Monitor/index.php
@@ -4,16 +4,28 @@
 <head>
     <meta charset="utf-8">
     <title>Monitor</title>
+    <link rel="stylesheet" href="Assets/Styles/boot.css" />
+    <link rel="stylesheet" href="Themes/MonitorTheme/style.css" />
 </head>
 
 <body>
     <header class="main_header">
-        <h1>Teste</h1>
+        <h1 class="main_header_logo">Monitor</h1>
     </header>
-    <main>
-        <form>
-            
-        </form>
+    <main class="main_content">
+        <aricle class="main_content_result">
+            <header>
+                <h1>Resultados</h1>
+            </header>
+        </aricle>
+        <article class="main_content_insert">
+            <header>
+                <h1>Inserir nova série</h1>
+            </header>
+            <form class="main_content_insert_form">
+                <input type="text" title="name" placeholder="Nome">
+            </form>
+        </article>
     </main>
 </body>
 
