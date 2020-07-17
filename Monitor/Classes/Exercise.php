63ad4410d1e264e1893f3474b6307e50ebbc9c41
JPSS14 <jp_scarabelli@hotmail.com>
2020-07-17 16:16:35 -0300

    Monitor UPDATE 11: Added exercise CRUD methods

--- Monitor/Classes/Exercise.php
+++ Monitor/Classes/Exercise.php
@@ -1,8 +1,8 @@
 <?php
 class Exercise{
     private $name;
-    private $regiao;
-    private $especialidade;
+    private $region;
+    private $specialty;
 
     public function getName(){
         return $this->name;
@@ -10,16 +10,75 @@ class Exercise{
     public function setName($name){
         $this -> name = $name;
     }
-    public function getRegiao(){
-        return $this->regiao;
+    public function getRegion(){
+        return $this->region;
     }
-    public function setRegiao($regiao){
-        $this -> regiao = $regiao;
+    public function setRegion($region){
+        $this -> region = $region;
     }
-    public function getEspecialidade(){
-        return $this->especialidade;
+    public function getSpecialty(){
+        return $this->specialty;
     }
-    public function setEspecialidade($especialidade){
-        $this -> especialidade = $especialidade;
+    public function setSpecialty($specialty){
+        $this -> specialty = $specialty;
+    }
+
+    // LIST ALL EXERCISES
+    public function returnAllExercise($cx){
+        $connection = $cx;
+        $select = "SELECT * FROM exercicio";
+        $result = mysqli_query($connection, $select);
+
+        if(!$result){
+            die("Erro no banco exercicio no método returnAllExercise");
+        }
+        return $result;
+    }
+
+    // ADD EXERCISE
+    public function addExercise($cx){
+        $connection = $cx;
+        $add = "INSERT INTO exercicio (nome, regiao, especialidade) ";
+        $add .= "VALUES ('{$this->getName()}','{$this->getRegion()}','{$this->getSpecialty}'";
+
+        $result = mysqli_query($add, $connection);
+
+        if(!$result){
+            die("Erro no banco exercicio no método addExercise");
+        }
+        return $result;
+    }
+
+    // UPDATE EXERCISE
+    public function updateExercise($cx, $idExerciseP){
+        $connection = $cx;
+        $idExercise = $idExerciseP;
+        $update = "UPDATE exercicio SET ";
+        $update .= "nome = '{$this->getName()}', ";
+        $update .= "regiao = '{$this->getRegion()}', ";
+        $update .= "especialidade = '{$this->getSpecialty()}' ";
+        $update .= "WHERE id_exercicio = $idExercise";
+
+        $result = mysqli_query($connection, $update);
+
+        if(!$result){
+            die("Erro no banco exercicio no método updateExercise");
+        }
+        return $result;
+    }
+
+    // DELETE EXERCISE
+    public function deleteExercise($cx, $idExerciseP){
+        $connection = $cx;
+        $idExercise = $idExerciseP;
+        $delete = "DELETE FROM exercicio ";
+        $delete .= "WHERE id_exercicio = {$idExercise}";
+
+        $result = mysqli_query($connection, $delete);
+
+        if(!$result){
+            die("Erro no banco exercicio no método deleteExercise");
+        }
+        return $result;
     }
 }
