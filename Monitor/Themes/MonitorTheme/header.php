0814fe316229081593cd610dad826bb923c495e6
JPSS14 <jp_scarabelli@hotmail.com>
2020-07-21 16:46:37 -0300

    Monitor UPDATE 14: Added nav in main header

--- Monitor/Themes/MonitorTheme/header.php
+++ Monitor/Themes/MonitorTheme/header.php
@@ -1,3 +1,29 @@
 <header class="main_header">
+    <!-- LOGO -->
     <h1 class="main_header_logo">Monitor</h1>
+    <!-- /LOGO -->
+
+    <!-- MENU -->
+    <div class="container">
+        <nav class="main_header_nav">
+            <ul class="main_header_nav_menu">
+                <li class="main_header_nav_menu_item"><a href="#" title="#">Exercicios</a>
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="#" title="">Add Exercicio</a></li>
+                        <li class="dropdown_item"><a href="#" title="">Update Exercicio</a></li>
+                        <li class="dropdown_item"><a href="#" title="">Delete Exercicio</a></li>
+                    </ul>
+                </li>
+                <li class="main_header_nav_menu_item"><a href="#" title="#">Series</a>
+                    <ul class="dropdown">
+                        <li class="dropdown_item"><a href="#" title="">Add Series</a></li>
+                        <li class="dropdown_item"><a href="#" title="">Update Series</a></li>
+                        <li class="dropdown_item"><a href="#" title="">Delete Series</a></li>
+                    </ul>
+                </li>
+                <li class="main_header_nav_menu_item"><a href="#" title="">Relatórios</a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- /MENU -->
 </header>
